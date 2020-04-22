0852885be6ff0985cfd62548b256bc1ca4c54b56
damaliboyd <damaliboyd@ymail.com>
2020-04-21 21:28:51 -0400

    project code

---
+++ js/data.js
@@ -0,0 +1,178 @@
+const data = [
+    {
+        action: "install fedora development tools",
+        time: 3,
+        prod: true
+    },
+    {
+        action: "singer.com",
+        time: 2,
+        prod: false
+    },
+    {
+        action: "last pass",
+        time: 1,
+        prod: true
+    },
+    {
+        action: "email",
+        time: 3,
+        prod: true
+    },
+    {
+        action: "edfinancial",
+        time: 2,
+        prod: true
+    },
+    {
+        action: "singer.com",
+        time: 1,
+        prod: false
+    },
+    {
+        action: "install fedora c development tools",
+        time: 3,
+        prod: true
+    },
+    {
+        action: "install mysql",
+        time: 3,
+        prod: true
+    },
+    {
+        action: "set up workspace",
+        time: 9,
+        prod: true
+    },
+    {
+        action: "configure github",
+        time: 10.5,
+        prod: true
+    },
+    {
+        action: "youtube",
+        time: 1,
+        prod: false
+    },
+    {
+        action: "set up workspace",
+        time: 45,
+        prod: true
+    },
+    {
+        action: "vscode",
+        time: 9,
+        prod: true
+    },
+    {
+        action: "debugging",
+        time: 2,
+        prod: true
+    },
+    {
+        action: "mysql workbench",
+        time: 10.5,
+        prod: true
+    },
+    {
+        action: "vscode",
+        time: 7,
+        prod: true
+    },
+    {
+        action: "canvas",
+        time: 6,
+        prod: true
+    },
+    {
+        action: "mysql workbench",
+        time: 10.5,
+        prod: true
+    },
+    {
+        action: "vscode",
+        time: 2,
+        prod: true
+    },
+    {
+        action: "setting up workspace",
+        time: 9,
+        prod: false
+    },
+    {
+        action: "mysql workbench",
+        time: 10.5,
+        prod: true
+    },
+    {
+        action: "vscode",
+        time: 2,
+        prod: true
+    },
+    {
+        action: "vscode",
+        time: 2,
+        prod: true
+    },
+    {
+        action: "github",
+        time: 1,
+        prod: true
+    },
+    {
+        action: "vscode",
+        time: 11,
+        prod: true
+    },
+    {
+        action: "github",
+        time: 1,
+        prod: true
+    },
+    {
+        action: "vscode",
+        time: 30,
+        prod: true
+    },
+    {
+        action: "mysql workbench",
+        time: 1.5,
+        prod: true
+    },
+    {
+        action: "google",
+        time: 5,
+        prod: true
+    },
+    {
+        action: "vscode",
+        time: .5,
+        prod: true
+    },
+    {
+        action: "slack",
+        time: 1,
+        prod: true
+    },
+    {
+        action: "vscode",
+        time: .5,
+        prod: true
+    },
+    {
+        action: "youtube",
+        time: 1.5,
+        prod: true
+    },
+    {
+        action: "notion.so",
+        time: 3,
+        prod: true
+    },
+    {
+        action: "vscode",
+        time: .5,
+        prod: true
+    },
+    
+  ]
